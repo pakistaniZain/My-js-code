8a869ec65d40a9e41e9c914aeb0b7e10cc8e95d5
Zain Taseer <135511096+pakistaniZain@users.noreply.github.com>
2024-02-02 22:08:43 +0000

    02_datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,4 @@
+// simple things that about use of variables    just basics 
+// const     we can't redeclare and reassign the value 
+// let       we can't redeclare it but we can reassign value in the case of let
+//  var  please do'nt use var as it is not compaatible with the modrens copping scenarios but also used in some old codes that why understaning is important 
