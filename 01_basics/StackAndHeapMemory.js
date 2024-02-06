dfbc059b3996cf82d01574164deb0fbaa43e41e9
Zain Taseer <135511096+pakistaniZain@users.noreply.github.com>
2024-02-06 06:45:24 +0000

    stach and heap memory

---
+++ 01_basics/StackAndHeapMemory.js
@@ -0,0 +1,2 @@
+// stack :   primitive   : simple you get cpy of declared variable : if we change will change in copy not in ordginal memory locatiobn 
+// heap  :   non-primitive   :  simple you get memory location of declared variable :everything change in original value 
